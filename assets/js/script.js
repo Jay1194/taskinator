8efb3acb08389e876bc410dcb637af90e0ea4049
github_Jay1194 <j.thomastt1@gmail.com>
2022-06-22 19:57:47 -0600

    Handle Form Submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 
 // DOM object reference and task item DOM reference //
-var buttonEl = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // 1. Created a new task item 2. Styled new task item 3. Added the text 4. Appended this element to the task list //
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
- // On a button click create a task! (uses "createTaskHandler" as a Callback function) //
-buttonEl.addEventListener("click", createTaskHandler);
+ // Click button or press enter key to submit (uses "createTaskHandler" as a Callback function) //
+formE1.addEventListener("submit", createTaskHandler);
