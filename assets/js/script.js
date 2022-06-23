73569d9e1c29887c77c8682ce014256740e18c99
github_Jay1194 <j.thomastt1@gmail.com>
2022-06-22 19:03:04 -0600

    Added Dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+// DOM object reference and task item DOM reference //
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+// 1. Created a new task item 2. Styled new task item 3. Added the text 4. Appended this element to the task list //
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+ // On a button click create a task! (uses "createTaskHandler" as a Callback function) //
 buttonEl.addEventListener("click", createTaskHandler);
